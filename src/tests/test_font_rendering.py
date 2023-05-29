5d2b7ac081c0b2b12c7c8d488a45e2d86f0da2f3
GbotHQ <tombrum9@gmail.com>
2023-05-29 13:23:18 +0200

    Update tests
    Update tests.

--- src/tests/test_font_rendering.py
+++ src/tests/test_font_rendering.py
@@ -5,37 +5,79 @@ import cv2 as cv
 from PIL import ImageFont
 
 from font_rendering import generate
+from bounding_box_math import calculate_char_bboxes
 
 
 class TestFontRenderer:
     def setup_class(self):
-        self.font = ImageFont.truetype("tests/assets/SilentReaction.ttf", 42)
+        self.font = ImageFont.truetype("tests/test_assets/SilentReaction.ttf", 42)
         self.input_text = "The quick brown fox"
 
-        self.img = cv.imread("tests/assets/test_img_font_rendering_generate.png")
-        self.alpha = cv.imread("tests/assets/test_alpha_font_rendering_generate.png")
+        self.img = cv.imread("tests/test_assets/test_img_font_rendering_generate.png")
+        self.alpha = cv.imread(
+            "tests/test_assets/test_alpha_font_rendering_generate.png"
+        )
 
-    def imshow(self, img):
-        cv.imshow("test", img)
-        cv.waitKey(1000)
-        cv.destroyAllWindows()
+    def generate_render_text_test_data(self):
+        seed(0)
+
+        (
+            text,
+            img,
+            font_size,
+            xy,
+            line_offsets,
+            padding,
+            font_color,
+            text_rotation_angle,
+            resolution_before_rotation,
+        ) = generate(self.input_text, self.font)
+
+        compression = [cv.IMWRITE_PNG_COMPRESSION, 7]
+        cv.imwrite(
+            "tests/test_assets/test_img_font_rendering_generate.png", img, compression
+        )
+        test_dict = {
+            "text": text,
+            "font_size": font_size,
+            "xy": xy,
+            "line_offsets": line_offsets,
+            "padding": padding,
+            "font_color": font_color,
+            "text_rotation_angle": text_rotation_angle,
+            "resolution_before_rotation": resolution_before_rotation,
+        }
+
+        print(test_dict)
+    
+    def test_character_bounding_boxes(self):
+        xy = [6, 21]
+        bboxes = calculate_char_bboxes(xy, self.input_text, self.font)
+        assert list(bboxes[0].keys()) == ["bbox", "mask", "char_index", "line_index"]
 
     def test_render_text(self):
         """Test if text rendering produces expected results"""
         seed(0)
-        text, img, alpha, bbox, font_size, font_color, text_rotation_angle = generate(
-            self.input_text, self.font
-        )
 
-        assert text == "The quick brown\nfox"
+        (
+            text,
+            img,
+            font_size,
+            xy,
+            line_offsets,
+            padding,
+            font_color,
+            text_rotation_angle,
+            resolution_before_rotation,
+        ) = generate(self.input_text, self.font)
 
         np.testing.assert_array_equal(img, self.img)
-        np.testing.assert_array_equal(alpha, self.alpha)
 
-        np.testing.assert_array_equal(
-            bbox.points,
-            np.array(((63, 97), (251, 57), (355, 547), (167, 587)), np.int32)
-        )
+        assert text == "The quick brown\nfox"
         assert font_size == 82
+        assert xy == [6, 21]
+        assert line_offsets == [0, 208]
+        assert padding == [62, 51, 38, 61]
         assert font_color.tolist() == [154, 77, 148]
         assert text_rotation_angle == -12
+        assert resolution_before_rotation == (508, 192)
