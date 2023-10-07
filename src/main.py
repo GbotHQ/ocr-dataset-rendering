d4e14928350301dd6b39c5573977e0bd9b2a83c0
GbotHQ <tombrum9@gmail.com>
2023-10-07 16:51:56 +0200

    Add a backup download source that is used when gdrive fails
    Add a backup download source that is used when downloading from Google Drive fails.

--- src/main.py
+++ src/main.py
@@ -1,20 +1,29 @@
-from pathlib import Path as pth
-import json
-import tempfile
-from shutil import rmtree
+def download_from_grdrive_or_mega(
+    path: Union[str, pth],
+    gdrive_file_id: str,
+    mega_file_id_and_key: str,
+    desc: Optional[str]=None,
+) -> pth:
+    path = pth(path)
+    if desc is None:
+        desc = str(path.stem)
 
-import fire
+    print(f"Downloading {desc}...")
 
-from prepare_data import (
-    get_text_dataset,
-    gdrive_download_and_extract,
-    get_hdris,
-    get_materials,
-    get_fonts,
-)
-from generate_samples_2d import generate_samples
-from blender_render_samples_3d import run_blender_command
-from postprocess import postprocess_samples
+    # download from google drive, if failed, download from mega
+    try:
+        download_path = gdrive_download_and_extract(path, gdrive_file_id)
+        if not download_path.is_dir():
+            raise DownloadError(f"{desc} from Google Drive")
+    except Exception as e:
+        print(e)
+        print(f"Attempting to download {desc} from Mega.nz")
+
+        download_path = mega_download_and_extract(path, mega_file_id_and_key)
+        if not download_path.is_dir():
+            raise DownloadError(f"{desc} from Mega.nz")
+
+    return download_path
 
 
 def download_and_prepare_data():
@@ -25,25 +34,32 @@ def download_and_prepare_data():
     print("Preparing text dataset...")
     _, texts_iter = get_text_dataset()
 
-    print("Downloading fonts...")
-    fonts_path = gdrive_download_and_extract(
-        download_path / "fonts", "1-K8EE0QsXfxaAV-5uOE6lhTLGicRZbW2"
+    fonts_path = download_from_grdrive_or_mega(
+        download_path / "fonts",
+        "1-K8EE0QsXfxaAV-5uOE6lhTLGicRZbW2",
+        "itg2TKoJ#UCOEQqX7pPwAf9pguWVUuksX7orWtzK5n4SdI6CQqGc"
     )
-    print("Downloading hdris...")
-    hdris_path = gdrive_download_and_extract(
-        download_path / "hdris", "1BNCTqw5fenCK-D48-a7VQ234Aq3k45hu"
+
+    hdris_path = download_from_grdrive_or_mega(
+        download_path / "hdris",
+        "1BNCTqw5fenCK-D48-a7VQ234Aq3k45hu",
+        "D5YWQKgR#fFRHe-HpbCc7-yAm3h5zxbR905o1hkrxKJDvQOsGOKk"
     )
-    print("Downloading materials...")
-    materials_path = gdrive_download_and_extract(
-        download_path / "materials", "1-5dz5DMce-braCrhVIsqB58PvcyB6qyy"
+
+    materials_path = download_from_grdrive_or_mega(
+        download_path / "materials",
+        "1-5dz5DMce-braCrhVIsqB58PvcyB6qyy",
+        "W0hGyCzB#-Gldvyt6uGt9D6iT8kDL-T4CKGNwIKD0Yc4jR2MAgxo"
     )
+
+    if not fonts_path.is_dir():
+        raise DownloadError("materials")
 
     fonts_iter = get_fonts(fonts_path)
     hdris_iter = get_hdris(hdris_path)
     materials_iter = get_materials(materials_path)
 
     return texts_iter, fonts_iter, hdris_iter, materials_iter
-
 
 def main(
     n_samples: int,
@@ -156,7 +172,3 @@ def main(
 
     print("Cleaning up temporary files...")
     rmtree(temp_dir)
-
-
-if __name__ == "__main__":
-    fire.Fire(main)
